036689a27fba82262e03370e7781f0190467123a
jmrecodes <john.moremm@gmail.com>
2024-06-04 08:32:26 +0800

    Hello, World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              John Moremm Abuyabor
+ *  Coursera User ID:  jmrecodes
+ *  Last modified:     June 4, 2024
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
